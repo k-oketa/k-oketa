762f690dbdcbe7608d037638d2cf9ed403bd3aac
k-oketa <cist.m220.k.oketa@gmail.com>
2021-09-17 11:56:17 +0900

    javaprog21の第二回の課題を追加

---
+++ javaprog21/JavaProg/src/lec01/Hello.java
@@ -0,0 +1,8 @@
+package lec01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("lec01.Hello");
+        System.out.println("World!");
+    }
+}
---
+++ javaprog21/JavaProg/src/lec01/Hikari.java
@@ -0,0 +1,10 @@
+package lec01;
+
+public class Hikari {
+    public static void main(String[] args) {
+        var code = "b1970010";
+        var name = "千歳 光";
+        System.out.println(code);
+        System.out.println(name);
+    }
+}
