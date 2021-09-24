8c6890e4fe36a8056c329e6e7492b30cd8ac1706
k-oketa <cist.m220.k.oketa@gmail.com>
2021-09-24 10:39:34 +0900

    javaprog21の第二回の発展課題を追加

--- javaprog21/JavaProg/src/lec01/GroupWork.java
+++ javaprog21/JavaProg/src/lec01/GroupWork.java
@@ -1,2 +1,22 @@
-package lec01;public class GroupWork {
+package lec01;
+
+public class GroupWork {
+
+    public static void main(String[] args) {
+        long a = 2L;
+        double b =  0.9d;
+        double c = 1.0d;
+        float d = 0.9f;
+
+        // longでキャストすることで1.1の小数点以下を切り捨てる
+        long longAns = (long) (a - b);
+        // 無限小数による誤差が生じる
+        double doubleAns1 = c - b;
+        // 桁数による誤差が生じる
+        double doubleAns2 = c - d;
+
+        System.out.println(longAns);
+        System.out.println(doubleAns1);
+        System.out.println(doubleAns2);
+    }
 }
