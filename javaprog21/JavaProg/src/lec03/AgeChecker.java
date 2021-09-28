14fb7e8bb5fe9be671a16c6bcdea33d1a3968cc6
k-oketa <cist.m220.k.oketa@gmail.com>
2021-09-28 15:12:00 +0900

    第三回の課題4を作成

--- javaprog21/JavaProg/src/lec03/AgeChecker.java
+++ javaprog21/JavaProg/src/lec03/AgeChecker.java
@@ -6,14 +6,17 @@ public class AgeChecker {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("年齢を入力してください: ");
-        int age = input.nextInt();
-        if (age < 0 || age >= 120) {
-            System.out.println("エラー");
-        } else if (age >= 20) {
-            System.out.println("成人");
-        } else {
-            System.out.println("未成年");
+        while (true) {
+            System.out.print("年齢を入力してください: ");
+            int age = input.nextInt();
+            if (age < 0 || age > 120) {
+                System.out.println("エラー");
+                break;
+            } else if (age >= 20) {
+                System.out.println("成人");
+            } else {
+                System.out.println("未成年");
+            }
         }
     }
 }
