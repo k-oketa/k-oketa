a87533408b8b011656e192ae11305d60d7aba860
k-oketa <cist.m220.k.oketa@gmail.com>
2021-09-28 15:33:44 +0900

    第三回のグループワーク課題を作成

---
+++ javaprog21/JavaProg/src/lec03/GroupWork.java
@@ -0,0 +1,13 @@
+package lec03;
+
+import java.util.Scanner;
+
+public class GroupWork {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("input: ");
+        String st = input.nextLine().replace(' ', '\n');
+        System.out.println(st);
+    }
+}
