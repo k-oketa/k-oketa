3d0171bc422b2e2e801153fc84ad537d606f2a99
k-oketa <cist.m220.k.oketa@gmail.com>
2021-09-28 15:27:27 +0900

    第三回の発展課題2を作成

--- javaprog21/JavaProg/src/lec03/Reader.java
+++ javaprog21/JavaProg/src/lec03/Reader.java
@@ -7,5 +7,8 @@ public class Reader {
         System.out.print("何かキーを押してください: ");
         int a = System.in.read(); // 入力したキーのバイトを取得
         System.out.println(a);
+        a = System.in.read();
+        System.out.println(a);
+        // ScannerはSystem.in.read()から取得したバイトを文字（nextIntなら数字、nextなら文字列のように）に変換する役割
     }
 }
