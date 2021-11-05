c831cc0fe812526df087733de75b051a9fdabe4b
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-05 19:11:58 +0900

    testMultiplication()のグリーンバーを確認

--- test-driven-development-by-example/the-money-example/src/test/java/money/MoneyTest.java
+++ test-driven-development-by-example/the-money-example/src/test/java/money/MoneyTest.java
@@ -2,11 +2,13 @@ package money;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MoneyTest {
 
     @Test
     void testMultiplication() {
-        var five = new Doller(5);
+        var five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
     }
