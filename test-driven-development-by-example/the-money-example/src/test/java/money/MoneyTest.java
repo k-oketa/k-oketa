731ada01a8a5066745c441cd3420190249959583
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-05 19:42:31 +0900

    更新後のtestEquality()のレッドバーを確認

--- test-driven-development-by-example/the-money-example/src/test/java/money/MoneyTest.java
+++ test-driven-development-by-example/the-money-example/src/test/java/money/MoneyTest.java
@@ -2,8 +2,7 @@ package money;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
 
@@ -20,5 +19,7 @@ public class MoneyTest {
     void testEquality() {
         var equals = new Dollar(5).equals(new Dollar(5));
         assertTrue(equals);
+        var notEquals = new Dollar(5).equals(new Dollar(6));
+        assertFalse(notEquals);
     }
 }
