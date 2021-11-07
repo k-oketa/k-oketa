0474f999393a7e6182805ab7f5bd153cea9dc69c
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-07 19:51:01 +0900

    testReduceMoney()のグリーンバーを確認

--- test-driven-development-by-example/the-money-example/src/main/java/money/Bank.java
+++ test-driven-development-by-example/the-money-example/src/main/java/money/Bank.java
@@ -2,6 +2,9 @@ package money;
 
 public class Bank {
     Money reduce(Expression source, String to) {
+        if (source instanceof Money) {
+            return (Money) source;
+        }
         var sum = (Sum) source;
         return sum.reduce(to);
     }
