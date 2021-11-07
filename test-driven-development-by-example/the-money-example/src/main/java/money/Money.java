d8c7a8212e31825d1555e721b907c9fba7e82623
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-07 21:52:34 +0900

    testMixedAddition()のグリーンバーを確認

--- test-driven-development-by-example/the-money-example/src/main/java/money/Money.java
+++ test-driven-development-by-example/the-money-example/src/main/java/money/Money.java
@@ -22,7 +22,7 @@ class Money implements Expression {
         return currency;
     }
 
-    Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
