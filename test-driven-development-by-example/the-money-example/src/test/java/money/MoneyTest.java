20ecff037306cd477d072ee23123e7674128c806
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-07 16:02:22 +0900

    testFrancMultiplier()のグリーンバーを確認

--- test-driven-development-by-example/the-money-example/src/test/java/money/MoneyTest.java
+++ test-driven-development-by-example/the-money-example/src/test/java/money/MoneyTest.java
@@ -20,4 +20,11 @@ public class MoneyTest {
         var notEquals = new Dollar(5).equals(new Dollar(6));
         assertFalse(notEquals);
     }
+
+    @Test
+    void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 }
