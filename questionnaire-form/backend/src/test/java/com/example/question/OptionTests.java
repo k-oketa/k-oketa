a58e0434647f6cc0e7a056ea9dae17d9a75a1b3e
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-08 22:55:32 +0900

    :white_check_mark: testSortOptions()のグリーンバー

--- questionnaire-form/backend/src/test/java/com/example/question/OptionTests.java
+++ questionnaire-form/backend/src/test/java/com/example/question/OptionTests.java
@@ -2,7 +2,11 @@ package com.example.question;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class OptionTests {
 
@@ -12,5 +16,21 @@ public class OptionTests {
         var two = new Option(2);
         assertEquals(one.compareTo(two), -1);
         assertEquals(two.compareTo(one), 1);
+    }
+
+    @Test
+    void testSortOptions() {
+        var tests = Arrays.asList(
+                new Option(1),
+                new Option(3),
+                new Option(4),
+                new Option(2));
+        var expected = Arrays.asList(
+                new Option(1),
+                new Option(2),
+                new Option(3),
+                new Option(4));
+        Collections.sort(tests);
+        assertIterableEquals(expected, tests);
     }
 }
