49fdabf02e66eb79daa6e68219235fb59a85b7c9
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-08 22:21:37 +0900

    :recycle: Questionクラスをリファクタリング

--- questionnaire-form/backend/src/main/java/com/example/question/Question.java
+++ questionnaire-form/backend/src/main/java/com/example/question/Question.java
@@ -1,21 +1,9 @@
 package com.example.question;
 
-public class Question implements Comparable<Question> {
-
-    int ordinal;
-
-    Question(int ordinal) {
-        this.ordinal = ordinal;
-    }
+public record Question(int ordinal) implements Comparable<Question> {
 
     @Override
     public int compareTo(Question o) {
-        if (ordinal < o.ordinal) {
-            return -1;
-        } else if (ordinal > o.ordinal) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return Integer.compare(ordinal, o.ordinal);
     }
 }
