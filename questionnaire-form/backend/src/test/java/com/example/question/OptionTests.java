0647072f6b89a55bda2e7170e36d3ed9786291d3
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-09 00:37:18 +0900

    :red_circle: testSortOption()のレッドバー

--- questionnaire-form/backend/src/test/java/com/example/question/OptionTests.java
+++ questionnaire-form/backend/src/test/java/com/example/question/OptionTests.java
@@ -1,5 +1,25 @@
 package com.example.question;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
 public class OptionTests {
 
+    @Test
+    void testSortOption() {
+        var actual = Arrays.asList(
+                new Option(1),
+                new Option(3),
+                new Option(4),
+                new Option(2));
+        var expected = Arrays.asList(
+                new Option(1),
+                new Option(2),
+                new Option(3),
+                new Option(4));
+        assertIterableEquals(expected, actual);
+    }
 }
