52b85dae6ca89a573f20b4c9b78ed90107f76920
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-09 19:49:25 +0900

    :white_check_mark: testSortQuestion()のグリーンバーを確認

--- questionnaire-form/backend/src/test/java/com/example/question/QuestionTests.java
+++ questionnaire-form/backend/src/test/java/com/example/question/QuestionTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Collections;
 
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
 public class QuestionTests {
 
     @Test
@@ -13,8 +15,14 @@ public class QuestionTests {
                 new SingleChoiceQuestion(1),
                 new MultipleChoiceQuestion(4),
                 new DescriptionQuestion(2),
-                new SingleChoiceDescriptionQuestion(2));
+                new SingleChoiceDescriptionQuestion(3));
+        var expected = Arrays.asList(
+                new SingleChoiceQuestion(1),
+                new DescriptionQuestion(2),
+                new SingleChoiceDescriptionQuestion(3),
+                new MultipleChoiceQuestion(4));
         Collections.sort(questions);
+        assertIterableEquals(expected, questions);
     }
 
 }
