8437ee8e20b74d46336d1b0359f2dba8319bbdef
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-16 14:48:40 +0900

    :+1:: 当たり判定を追加

--- UnityNoKyokasyo2020/CatEscape/Assets/ArrowController.cs
+++ UnityNoKyokasyo2020/CatEscape/Assets/ArrowController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
+    private GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -17,6 +19,18 @@ public class ArrowController : MonoBehaviour
         transform.Translate(0, -0.1f, 0);
 
         if (transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        var p1 = transform.position;
+        var p2 = this.player.transform.position;
+        var dir = p1 - p2;
+        var d = dir.magnitude;
+        var r1 = 0.5f;
+        var r2 = 1.0f;
+
+        if (d < r1 + r2)
         {
             Destroy(gameObject);
         }
