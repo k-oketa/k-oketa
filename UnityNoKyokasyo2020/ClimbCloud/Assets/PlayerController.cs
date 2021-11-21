43532fb502075e1bb0139e33adc35bd022ebe1fe
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-22 03:07:50 +0900

    :+1:: ClearSceneを追加

--- UnityNoKyokasyo2020/ClimbCloud/Assets/PlayerController.cs
+++ UnityNoKyokasyo2020/ClimbCloud/Assets/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -48,5 +49,6 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Goal!");
+        SceneManager.LoadScene("ClearScene");
     }
 }
