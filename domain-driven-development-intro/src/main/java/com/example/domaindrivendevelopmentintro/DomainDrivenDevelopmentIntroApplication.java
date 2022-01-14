54629bc7e4ba9adbc6daff42f22e1529f6a43f7c
k-oketa <cist.m220.k.oketa@gmail.com>
2022-01-14 10:42:06 +0900

    :sparkles: プロジェクトの作成

---
+++ domain-driven-development-intro/src/main/java/com/example/domaindrivendevelopmentintro/DomainDrivenDevelopmentIntroApplication.java
@@ -0,0 +1,13 @@
+package com.example.domaindrivendevelopmentintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenDevelopmentIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainDrivenDevelopmentIntroApplication.class, args);
+    }
+
+}
