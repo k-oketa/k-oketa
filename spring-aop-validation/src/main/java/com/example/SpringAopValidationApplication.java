87e9cd23039795313a276fed5d7f4c881aacb526
k-oketa <cist.m220.k.oketa@gmail.com>
2022-02-15 21:08:09 +0900

    :tada: 新規プロジェクトを作成

---
+++ spring-aop-validation/src/main/java/com/example/SpringAopValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopValidationApplication.class, args);
+    }
+
+}
